4c268ae06daddc2b88cb107e61005bb0905672b6
yosefsantiago <yosef.santiago@upr.edu>
2026-03-06 13:39:29 -0400

    network first broke offline availability

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -15,17 +15,6 @@ self.addEventListener("install", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        // SUCCESS: Put a copy of the fresh file into the cache
-        return caches.open(CACHE).then(cache => {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      })
-      .catch(() => {
-        // OFFLINE: Return the cached version if network fails
-        return caches.match(event.request);
-      })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
