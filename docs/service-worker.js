9c15bfe0dc21c89d3f3dea9533f0aad2d082e442
yosefsantiago <yosef.santiago@upr.edu>
2026-03-06 12:28:31 -0400

    more version update testing

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -15,9 +15,17 @@ self.addEventListener("install", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    // try network, if it fails use cached files
-    fetch(event.request) 
-      .then(response => response)
-      .catch(() => caches.match(event.request))
+    fetch(event.request)
+      .then(response => {
+        // SUCCESS: Put a copy of the fresh file into the cache
+        return caches.open(CACHE).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => {
+        // OFFLINE: Return the cached version if network fails
+        return caches.match(event.request);
+      })
   );
 });
